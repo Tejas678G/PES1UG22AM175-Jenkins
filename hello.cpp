000ebf76b30c098ef7349fc15f4d3dfddd6449ec
PES1UG22AM175 <149988628+Tejas678G@users.noreply.github.com>
2025-03-11 12:32:30 +0530

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,7 @@
-#include <iostream> 
-using namespace std; 
-int main() { 
-    cout << "Hello, Jenkins! Webhook test successful!!" << endl; 
-    return 0; 
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, Jenkins Pipeline!" << endl;
+    return 0;
 }
